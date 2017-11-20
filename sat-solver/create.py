c155ef4064c6695bd397464ccdd4dacc4db3375f
tomabou <hoge@hoge>
2017-11-20 11:49:01 +0900

    better creat.py

--- sat-solver/create.py
+++ sat-solver/create.py
@@ -1,54 +1,44 @@
-size = 5
-target = [6,6,6,7,7,7]
+size = 3
+target = [1,1,1,1,1,2]
 l = len(target)
 s = "; sp-1 problme\n"
 
-for i in range(size):
-    for j in range(i+size):
-        s+= "(int n_{}_{} 0 {})\n".format(i,j,l)
-
-for i in range(size-1):
-    for j in range(size*2 - 2-i):
-        s+= "(int n_{}_{} 0 {})\n".format(i+size,j,l)
+def is_member(p,q):
+    return (0<=p<=size*2-2)and(0<=q<size*2-1)and(-size<p-q<size)
+for i in range(2*size+1):
+    for j in range(2*size+1):
+        if is_member(i,j):
+            s+= "(int n_{}_{} 0 {})\n".format(i,j,l)
 
 vals = ""
-for i in range(size):
-    for j in range(i+size):
-        vals+= "n_{}_{} ".format(i,j)
-
-for i in range(size-1):
-    for j in range(size*2 - 2-i):
-        vals+= "n_{}_{} ".format(i+size,j)
+for i in range(2*size+1):
+    for j in range(2*size+1):
+        if is_member(i,j):
+            vals+= "n_{}_{} ".format(i,j)
 
 for i in range(len(target)):
     s+="(count {} ({}) eq {})\n".format(i+1 , vals, target[i])
-
-
+    
 def condition(a,b,p,q):
     return "(or (= n_{}_{} 0) (= n_{}_{} 0) (= n_{}_{} n_{}_{}))\n".format(a,b,p,q,a,b,p,q)
 
 s+="\n"
-for i in range(size):
-    for j in range(i+size-1):
-        s+=condition(i,j,i,j+1)
-for i in range(size-1):
-    for j in range(size*2 - 3-i):
-        s+=condition(i+size,j,i+size,j+1)
+for i in range(2*size+1):
+    for j in range(2*size+1):
+        if is_member(i,j)and is_member(i,j+1):
+            s+=condition(i,j,i,j+1)
 s+="\n"
-for i in range(size-1):
-    for j in range(i+size):
-        s+=condition(i,j,i+1,j)
-for i in range(size-1):
-    for j in range(size*2 - 2-i):
-        s+=condition(i+size-1,j,i+size,j)
-s+="\n"
-for i in range(size-1):
-    for j in range(i+size):
-        s+=condition(i,j,i+1,j+1)
-for i in range(size-1):
-    for j in range(i+size):
-        s+=condition(size*2-2-i,j,size*2-3-i,j+1)
 
+for i in range(2*size+1):
+    for j in range(2*size+1):
+        if is_member(i,j)and is_member(i+1,j):
+            s+=condition(i,j,i+1,j)
+s+="\n"
+
+for i in range(2*size+1):
+    for j in range(2*size+1):
+        if is_member(i,j)and is_member(i+1,j+1):
+            s+=condition(i,j,i+1,j+1)
 s+=";END\n"
 f = open('text.txt', 'w') 
 
