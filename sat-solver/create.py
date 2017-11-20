b5fb7f8403c4792b11db6d51b575b1c50756f928
tomabou <hoge@hoge>
2017-11-20 20:15:09 +0900

    write create.py

--- sat-solver/create.py
+++ sat-solver/create.py
@@ -1,5 +1,5 @@
 size = 3
-target = [1,1,1,1,1,2]
+target = [1,2]
 l = len(target)
 s = "; sp-1 problme\n"
 
@@ -9,6 +9,68 @@ for i in range(2*size+1):
     for j in range(2*size+1):
         if is_member(i,j):
             s+= "(int n_{}_{} 0 {})\n".format(i,j,l)
+
+def nexts(p):
+    a = p[0]
+    b = p[1]
+    return [(a-1,b-1),(a-1,b),(a,b-1),(a+1,b),(a+1,b+1),(a,b+1)]
+
+
+def groups(n):
+    if n==1:
+        return [[(0,0)]]
+    else:
+        pre = groups(n-1)
+        ans = set()
+        for g in pre:
+            for p in g:
+                for new in nexts(p):
+                    l = list(g)
+                    if new not in l:
+                        l.append(new)
+                        l.sort()
+                        mx = 0
+                        my = 0
+                        for p in l:
+                            mx = min(mx,p[0])
+                            my = min(my,p[1])
+                        nl = [(p[0]-mx,p[1]-my) for p in l]
+                        ans.add(tuple(nl))
+        return list(ans)
+
+for g in groups(3):
+    print(g)
+
+def group_comdition(x,y,group,index):
+    for pos in group:
+        if not is_member(x+pos[0],y+pos[1]):
+            return False
+    cnd = "  (and\n"
+    bd = [[False for i in range(size*2+1)] for j in range(size*2+1)]    
+    for pos in group:
+        bd[x+pos[0]][y+pos[1]] = True
+    for i in range(size*2-1):
+        for j in range(size*2-1):
+            if is_member(i,j):
+                if bd[i][j]:
+                    cnd += "    ( = n_{}_{} {})\n".format(x+pos[0],y+pos[1],index)
+                else:
+                    cnd += "    (!= n_{}_{} {})\n".format(x+pos[0],y+pos[1],index)
+    cnd += "  )\n"
+    return cnd
+
+s += "( or\n"
+for index in range(len(target)):
+    gs = groups(target[index])
+    for g in gs:
+        for i in range(2*size+1):
+            for j in range(2*size +1):
+                cnd = group_comdition(i,j,g,index+1)
+                if False == cnd:
+                    continue
+                else:
+                    s += cnd
+s+=")\n"
 
 vals = ""
 for i in range(2*size+1):
