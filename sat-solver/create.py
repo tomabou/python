4eb93d11a0c849316f571adf9b7fcb0c746699c0
HomePC <kazu.8182.9.6@gmail.com>
2017-11-21 01:20:43 +0900

    easy is easy

--- sat-solver/create.py
+++ sat-solver/create.py
@@ -1,5 +1,5 @@
 size = 5
-target = [6,6,6,5,5]
+target = [6,6,6,6,8]
 filename = "easy.txt"
 l = len(target)
 s = "; sp-1 problme\n"
@@ -52,8 +52,8 @@ def group_comdition(x,y,group,index):
             if is_member(i,j):
                 if bd[i][j]:
                     cnd += "    ( = n_{}_{} {})\n".format(i,j,index)
-                else:
-                    cnd += "    (!= n_{}_{} {})\n".format(i,j,index)
+#                else:
+#                    cnd += "    (!= n_{}_{} {})\n".format(i,j,index)
     cnd += "  )\n"
     return cnd
 
