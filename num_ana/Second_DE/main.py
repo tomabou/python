4564fea9782cb75e35ff6e4a679312426745d7b3
tomabou <hoge@hoge>
2018-02-09 17:03:25 +0900

    backward_eular

--- num_ana/Second_DE/main.py
+++ num_ana/Second_DE/main.py
@@ -17,11 +17,27 @@ def forward_euler(h,T):
         q = q1
     return (p_res,q_res)
 
+def backward_euler(h,T):
+    p = 1
+    q = 0  
+    p_res = []
+    q_res = []
+
+    for i in range(100):
+        p_res.append(p)
+        q_res.append(q)
+        p1 = (p - h*q)/(1 + h*h)
+        q1 = (h*p + q) / (1 + h*h)
+        p = p1
+        q = q1
+    return (p_res,q_res)
 
 if __name__ =="__main__":
     h = 0.1
 
     (P,Q) = forward_euler(h,10)
+    plt.scatter(P,Q)
+    (P,Q) = backward_euler(h,10)
     plt.scatter(P,Q)
     for i in range(1000):
         filename= "./image/output_{}.png".format(i)
