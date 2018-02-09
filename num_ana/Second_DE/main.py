c8f7a588a46564e2fcff0da5db74702ef7654a1e
tomabou <hoge@hoge>
2018-02-09 15:11:26 +0900

    output image

--- num_ana/Second_DE/main.py
+++ num_ana/Second_DE/main.py
@@ -1,31 +1,21 @@
-from methods import *
 import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
+import os.path
 
 if __name__ =="__main__":
-    N = 100
-    c = 2
-    b = 2*rand(N)-1
-    loop = 105
-    w = optw(N,c)
-    
-    result4 = SOR(N,c,b,loop,w)
-    result5 = RBSOR(N,c,b,loop,w)
- 
-    
-    matrix = np.empty((0,N))
-    for vec in result4:
-        vec = np.log10(np.abs(vec))
-        matrix = np.vstack((matrix,vec.reshape(1,N)))
-
-    matrix = matrix.T        
+    matrix = np.random.rand(20,20)
     print(matrix.shape)
     sns.heatmap(matrix)
-    filename= "matrix_N={}_c={}_w={}_loop={}.png".format(N,c,w,loop)
+    for i in range(1000):
+        filename= "./image/output_{}.png".format(i)
+        print(filename)
+        print(os.path.exists(filename))
+        if not os.path.exists(filename):
+            break
+
     plt.savefig(filename, bbox_inches='tight')
 
-    # グラフを表示する
     plt.show()
 
     
