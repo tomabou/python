262232a3821582e92f328f6a67b5a0c54dd6ad89
tomabou <hoge@hoge>
2018-02-10 10:04:53 +0900

    daikei

--- num_ana/Second_DE/main.py
+++ num_ana/Second_DE/main.py
@@ -31,14 +31,45 @@ def backward_euler(h,T):
         p = p1
         q = q1
     return (p_res,q_res)
+def symplectic(h,T):
+    p = 1
+    q = 0  
+    p_res = []
+    q_res = []
 
+    for i in range(100):
+        p_res.append(p)
+        q_res.append(q)
+        p1 = (p - h*q)
+        q1 = (h*p1 + q)
+        p = p1
+        q = q1
+    return (p_res,q_res)
+def dai(h,T):
+    p = 1
+    q = 0  
+    p_res = []
+    q_res = []
+
+    for i in range(100):
+        p_res.append(p)
+        q_res.append(q)
+        p1 = ((1-h*h/4)*p - h*q) / (1 + h*h/4)
+        q1 = (h*p + (1-h*h/4)*q) / (1 + h*h/4)
+        p = p1
+        q = q1
+    return (p_res,q_res)
 if __name__ =="__main__":
-    h = 0.1
+    h = 0.1 
 
     (P,Q) = forward_euler(h,10)
     plt.scatter(P,Q,label = "forward_euler")
     (P,Q) = backward_euler(h,10)
     plt.scatter(P,Q,label = "backward_eular")
+    (P,Q) = symplectic(h,10)
+    plt.scatter(P,Q,label = "symplectic")
+    (P,Q) = dai(h,10)
+    plt.scatter(P,Q,label = "dai")
     for i in range(1000):
         filename= "./image/output_{}.png".format(i)
         if not os.path.exists(filename):
