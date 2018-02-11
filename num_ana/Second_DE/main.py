5888791408f6ebfff33354a68160c7e09f8efcee
tomabou <hoge@hoge>
2018-02-11 16:16:04 +0900

    consideration

--- num_ana/Second_DE/main.py
+++ num_ana/Second_DE/main.py
@@ -27,7 +27,6 @@ def backward_euler(h,T):
     q = 0  
 
     loop = int(T/h)
-
     p_res = np.ndarray(loop)
     q_res = np.ndarray(loop)
 
@@ -100,26 +99,27 @@ def runge_kutta(h,T):
         q = q + h*(q1/6 + q2/3+q3/3+q4/6)
     return (p_res,q_res)
 if __name__ =="__main__":
-    h = 0.3 
-    time = 100000
+    h = 0.1
+    time = 10
 
-    size = 1
+    size = 10
+    lim = 1.5
 
     plt.figure(figsize=(9,9))
-    plt.xlim(-1.5,1.5)
-    plt.ylim(-1.5,1.5)
-    #(P,Q) = forward_euler(h,time)
-    #plt.scatter(P,Q,label = "forward_euler",s=size)
-    #(P,Q) = backward_euler(h,time)
-    #plt.scatter(P,Q,label = "backward_eular",s = size)
-    #(P,Q) = symplectic(h,time)
-    #plt.scatter(P,Q,label = "symplectic", s = size)
-    #(P,Q) = dai(h,time)
-    #plt.scatter(P,Q,label = "dai", s=size)
+    plt.xlim(-lim,lim)
+    plt.ylim(-lim,lim)
+    (P,Q) = forward_euler(h,time)
+    plt.scatter(P,Q,label = "forward_euler",s=size)
+    (P,Q) = backward_euler(h,time)
+    plt.scatter(P,Q,label = "backward_eular",s = size)
+    (P,Q) = symplectic(h,time)
+    plt.scatter(P,Q,label = "symplectic", s = size)
+    (P,Q) = dai(h,time)
+    plt.scatter(P,Q,label = "dai", s=size)
     (P,Q) = runge_kutta(h,time)
     plt.scatter(P,Q,label = "runge_kutta", s=size)
     for i in range(1000):
-        filename= "./image/output_{}.png".format(i)
+        filename= "./image/output_h={}_{}.png".format(h,i)
         if not os.path.exists(filename):
             break
 
