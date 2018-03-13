2ce009a32f0d59dc6a160580b88f42764fe55e97
tomabou <kazu.8128.9.6@gmail.com>
2018-03-13 21:29:18 +0900

    soundclondに投稿したときのコード

--- music/infinit.py
+++ music/infinit.py
@@ -16,34 +16,26 @@ def create_sound(h,l,BPM,framerate,v):
     hertz = 442 * (2**(h/12))
     x = x/(framerate/hertz)
     x = x - np.floor(x)
-    return v * san_func(x)
+    return v * sin_func(x)
 
 def tolen(n,BPM,framerate):
     return int(n*60/BPM*framerate)
 
 if __name__=="__main__":
     framerate = 44100
-    BPM = 50
+    BPM = 150
     hertz = 220
+    total = 100
 
     """hight,len,start"""
-    l =  int(25*60/BPM*framerate)
+    l =  int((total+1)*60/BPM*framerate)
     x = np.zeros(l)
-    for i in range(24):
+    for i in range(total):
         start = int(i*60/BPM*framerate)
-        for j in range(-5,5):
+        for j in range(-10,10):
             h = j*12 + i%12
-            s = create_sound(h,1,BPM,framerate,1/(abs(h)+10))
+            s = create_sound(h,1,BPM,framerate,np.exp(-h*h / 2000))
             x[start:start+len(s)] += s
-
-#    N = len(x)
-#    fc = 2000/framerate
-#    F = np.fft.fft(x)/(N/2)
-#    freq = np.fft.fftfreq(len(x))
-#    F[0] = F[0]/2
-#    F[(freq > fc)] = 0
-#    F[(freq < 0)] = 0
-#    x = np.fft.ifft(F)*(2*N/2)
 
     m = np.max(abs(x))
     x = (x*30000/m).astype(np.int16)
