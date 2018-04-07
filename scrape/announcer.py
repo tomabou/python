0df35bd168538fe086bd18d16aaec4f43aff1b60
tomabou <kazu.8128.9.6@gmail.com>
2018-04-08 02:04:50 +0900

    生年月日と配偶者get

--- scrape/announcer.py
+++ scrape/announcer.py
@@ -4,12 +4,37 @@ from urllib.request import urlopen
 import sys
 import pandas as pd
 
-print(sys.getdefaultencoding())
+def getInfo(name):
+    name = urllib.parse.quote_plus(name)
+    url = "https://ja.wikipedia.org/wiki/"+name
+    html = urlopen(url)
+    soup = BeautifulSoup(html,"lxml")
+    res = soup.find("table",class_="infobox")
+    df = pd.read_html(str(res))
+    df = df[0]
+    data = df[df[0]=="生年月日"]
+    birthday="nd"
+    if len(data)==1:
+        birthday= data[1].values[0]
+    hasband = "nd"
+    data = df[df[0]=="配偶者"]
+    if len(data)==1:
+        hasband=data[1].values[0]
+    
+    return (birthday,hasband)
 
-name = "江藤愛"
-name = urllib.parse.quote_plus(name)
-url = "https://ja.wikipedia.org/wiki/"+name
-#url = "https://matome.naver.jp/odai/2138128445557718901"
-html = urlopen(url)
-soup = BeautifulSoup(html,"lxml")
-print(soup)
+def announcerNames():
+    names = ["江藤愛"]
+    return names
+
+if __name__=="__main__":
+    names = announcerNames()
+    birthdays=[]
+    hasbands=[]
+    for name in names:
+        b,h=getInfo(name)
+        birthdays.append(b)
+        hasbands.append(h)
+    dic = {"birhday":birthdays, "hasband":hasbands, "name":names}
+    df = pd.DataFrame.from_dict(dic)
+    print(df.to_csv())
