561312983ac03738d72d0d731066aff321de60c3
tomabou <kazu.8128.9.6@gmail.com>
2018-05-03 02:09:40 +0900

    debag baikai

--- graph/baikai.py
+++ graph/baikai.py
@@ -44,14 +44,23 @@ def calc():
             edges[x1].append(x2)
             edges[x2].append(x1)
 
+    c = 10
+    edges=[]
+    for i in range(10):
+        edges.append([])
+    for i in range(8):
+        edges[i].append(i+1)
+        edges[i+1].append(i)
+    edges[3].append(4)
+    edges[4].append(3)
+        
 
     baikai = np.zeros(c)
     #j = 4544
     for j in range(c):
-        if not j in id_to_stcd:
-            continue
+#        if not j in id_to_stcd:
+#            continue
         d = np.zeros(c, dtype=int)
-        g = np.zeros(c,dtype=int)
         que = collections.deque()
         que.append(j)
         while len(que) > 0:
@@ -61,7 +70,16 @@ def calc():
                     d[nv] = d[v] +1
                     que.append(nv)
 
+        print("d   ",end='')
+        for i in range(c):
+            print(d[i],end = ' ')
+        print("")
+
+
+        g = np.zeros(c,dtype=int)
+        used = np.zeros(c,dtype = int)
         g[j] = 1
+        used[j]=1
         que = collections.deque()
         que.append(j)
         while len(que) > 0:
@@ -69,31 +87,50 @@ def calc():
             for nv in edges[v]:
                 if d[nv] == d[v]+1:
                     g[nv] += g[v]
+                if used[nv]==0:
                     que.append(nv)
+                    used[nv] = 1
+                    
+        print("g   ",end='')
+        for i in range(c):
+            print(g[i],end = ' ')
+        print("")
         
         maxlen = np.max(d)
         depthlist = []
         for i in range(maxlen+1):
             depthlist.append([])
         for i in range(c):
-            depthlist[d[i]].append(i)
+            if d[i] > 0:
+                depthlist[d[i]].append(i)
+        
+        for i in range(len(depthlist)):
+            print("{} is".format(i),end=' ')
+            for v in depthlist[i]:
+                print(v,end=' ')
+            print("")
+        print("\n")
 
+        new_baikai = np.zeros(c)
         for vs in reversed(depthlist):
             for v in vs:
                 for nv in edges[v]:
                     if d[nv] == d[v] + 1:
-                        baikai[v] += (baikai[nv]) * g[v]/g[nv]
+                        new_baikai[v] += (new_baikai[nv] + 1) * g[v]/g[nv]
+        
+        baikai += new_baikai
 
-        m = 0
-        x = 0
-        for i in range(c):
-            #if m < d[i]:
-            #    m = d[i]
-            #    x = i
-            m += d[i]
-        sname1 = stations[stations["station_cd"] == id_to_stcd[j]].iat[0,2]
-        print(sname1,end=" ")
-        print(m)
+    for i in range(c):
+        print(baikai[i])
+
+    m = 0
+    x = 0
+    for i in range(c):
+        if m < baikai[i]:
+            m = baikai[i]
+            x = i
+    #sname1 = stations[stations["station_cd"] == id_to_stcd[x]].iat[0,2]
+    #print(sname1,end=" ")
 
 if __name__ == "__main__":
     calc()
