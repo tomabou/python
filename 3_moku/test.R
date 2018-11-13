387a57a574442539972162230e47854fea57349a
tomabo <kazu.8128.9.6@gmail.com>
2018-11-14 02:00:11 +0900

    add code

--- 3_moku/test.R
+++ 3_moku/test.R
@@ -1,14 +1,17 @@
 .libPaths(c(.libPaths(),"/home/tomabo/R/x86_64-pc-linux-gnu-library/3.5"))
 seto<-subset(iris[1:50,],select=-Species)
+versi<-subset(iris[51:100,],select=-Species)
 virgi<-subset(iris[101:150,],select=-Species) 
 
 seto.m<-apply(seto[1:45,],2,mean)
+versi.m<-apply(versi[1:45,],2,mean)
 virgi.m<-apply(virgi[1:45,],2,mean)
 seto.v<-var(seto[1:45,])
+versi.v<-var(versi[1:45,])
 virgi.v<-var(virgi[1:45,],) 
 
+
 mul <- function(x){
-    print(x)
     x[1:4] %*% x[5:8]
 }
 
@@ -23,13 +26,38 @@ maha <- function(x,y,S){
     apply(cbind(d,k), 1, mul)
 }
 
-D1<-maha(seto[46:50,],seto.m,seto.v)
-D2<-maha(seto[46:50,],virgi.m,virgi.v) 
+disc <- function(A,B){
+    A.m<-apply(A[1:45,],2,mean)
+    B.m<-apply(B[1:45,],2,mean)
+    A.v<-var(A[1:45,])
+    B.v<-var(B[1:45,])
+    data <- rbind(A[1:45,],B[1:45,])
+    data.v<- var(data)
 
-print(cbind(D1,D2))
+    D1<-maha(A[46:50,],A.m,A.v)
+    D2<-maha(A[46:50,],B.m,B.v) 
 
-#46 2.1752192 137.9376
-#47 2.8163645 173.8815
-#48 1.4346178 142.1425
-#49 1.2398930 182.5972
-#50 0.4700029 160.2070
+    a <- cbind(D1,D2)
+
+    D1<-maha(B[46:50,],A.m,A.v)
+    D2<-maha(B[46:50,],B.m,B.v) 
+
+    b <- cbind(D1,D2)
+    print(rbind(a,b))
+
+    D1<-maha(A[46:50,],A.m,data.v)
+    D2<-maha(A[46:50,],B.m,data.v) 
+
+    a <- cbind(D1,D2)
+
+    D1<-maha(B[46:50,],A.m,data.v)
+    D2<-maha(B[46:50,],B.m,data.v) 
+
+    b <- cbind(D1,D2)
+    print(rbind(a,b))
+
+}
+
+disc(seto,versi)
+disc(seto,virgi)
+disc(versi,virgi)
