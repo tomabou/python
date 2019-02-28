72a70f0b8714e36067cc991e81f93cc7aefaf842
tomabo <kazu.8128.9.6@gmail.com>
2019-03-01 03:48:16 +0900

    can draw

--- string/sound.py
+++ string/sound.py
@@ -1,7 +1,8 @@
-
+import matplotlib.animation as animation
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import convolve2d
+import numba
 
 
 def initialize(n, m):
@@ -15,6 +16,7 @@ def initialize(n, m):
     return (Vx, Vy, P, ax, ay, b)
 
 
+# @numba.jit
 def calc_NVx(dx, dt, ax, Vx, P):
     # 畳み込みなのでひっくり返ることに注意
     Px = convolve2d([[1], [-1]], P, mode='full') / dx
@@ -23,6 +25,7 @@ def calc_NVx(dx, dt, ax, Vx, P):
     return NVx / (1/dt + ax/2)
 
 
+# @numba.jit
 def calc_NVy(dy, dt, ay, Vy, P):
     # 畳み込みなのでひっくり返ることに注意
     Px = convolve2d([[1, -1]], P, mode='full') / dy
@@ -31,6 +34,7 @@ def calc_NVy(dy, dt, ay, Vy, P):
     return NVx / (1/dt + ay/2)
 
 
+# @numba.jit
 def calc_NP(dx, dy, dt, Vx, Vy, P, b, k):
     Vxx = convolve2d([[1], [-1]], Vx, mode='full') * k / dx
     Vxx = Vxx[:-1, :]
@@ -40,6 +44,7 @@ def calc_NP(dx, dy, dt, Vx, Vy, P, b, k):
     return NP/(1/dt + b/2)
 
 
+# @numba.jit
 def leapfrog(dx, dy, dt, ax, ay, b, Vx, Vy, P, k):
     NVx = calc_NVx(dx, dt, ax, Vx, P)
     NVy = calc_NVy(dy, dt, ay, Vy, P)
@@ -61,8 +66,40 @@ def test():
     k = 0.1
     for i in range(10):
         print(P)
+
         Vx, Vy, P = leapfrog(dx, dy, dt, ax, ay, b, Vx, Vy, P, k)
 
 
+def draw_test():
+    np.set_printoptions(precision=3, suppress=True)
+    Vx, Vy, P, ax, ay, b = initialize(100, 100)
+    for i in range(35, 40):
+        for j in range(35, 40):
+            P[i, j] = 1
+    dx = 0.1
+    dy = 0.1
+    dt = 0.1
+    k = 0.1
+    fig = plt.figure()
+    ims = []
+    for i in range(10):
+        print(P)
+
+        Vx, Vy, P = leapfrog(dx, dy, dt, ax, ay, b, Vx, Vy, P, k)
+        im = plt.imshow(P, animated=True)
+        ims.append([im])
+    ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
+                                    repeat_delay=1000)
+
+    plt.show()
+
+
+import cProfile
 if __name__ == "__main__":
-    test()
+    #pr = cProfile.Profile()
+    # pr.enable()
+    # test()
+    draw_test()
+    # pr.disable()
+    # pr.print_stats()
+    # pr.dump_stats('fib.profile')
